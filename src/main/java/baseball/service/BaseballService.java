cf27774bfd209ce9bb90725bf479230f45fd33a2
jhsseonn <hosu0125@gmail.com>
2022-10-25 20:55:19 +0900

    feat: get user number

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,5 +1,6 @@
 package baseball.service;
 
+
 import baseball.domain.Baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
@@ -7,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseballService {
 
     int[] randomNum=new int[3];
+    int[] userNum=new int[3];
     Baseball baseball;
 
     public void startBaseballGame() {
@@ -14,6 +16,15 @@ public class BaseballService {
             randomNum[i]=Randoms.pickNumberInRange(1,9);
         }
         baseball.setComNum(randomNum);
+    }
+
+    public void getUserNum() {
+        System.out.println("숫자를 입력해주세요: ");
+        String number=Console.readLine();
+        for(int i=0; i<number.length(); i++){
+            userNum[i]=number.charAt(i)-'0';
+        }
+        baseball.setUserNum(userNum);
     }
 
 
